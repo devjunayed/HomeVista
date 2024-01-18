4ac090a012cbaca219ccf64ad2da558e73802a37
adiyat-shihab <mohammadshihab104@gmail.com>
2024-01-18 18:29:18 +0600

    create register and login page implement firebase auth also create navbar. create backend api with connected mongobd. when user regitser user name and email data store in database

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/component/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        lightBlue: "#D3F4FB",
+        regularBlue: "#9DE3F6",
+        mediumBlue: "#59D7EE",
+        highBlue: "#53DFD1",
       },
     },
   },
-  plugins: [require("daisyui")],
-  daisyui: {
-    themes: ["light"],
-  },
-}
+  plugins: [],
+};
