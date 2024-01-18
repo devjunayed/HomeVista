7141dcd1ccc584515657f9159c5634af4b631c17
iamtanim0195 <sahadatmd195@gmail.com>
2024-01-18 17:18:46 +0600

    basic

---
+++ src/app/SignUp/layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Slayout = ({ children }) => {
+    return (
+        <>
+            <div>layout</div>
+            {children}
+        </>
+    );
+}
+
+export default Slayout;
