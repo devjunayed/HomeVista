43bb75f7554642f80cb3146654aa9973cdbac305
iamtanim0195 <sahadatmd195@gmail.com>
2024-01-19 11:14:01 +0600

    fixe banner

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ import Banner from "@/components/Banner/Banner";
 
 export default function Home() {
   return <>
-    <div className='pt-24 min-h-[calc(100vh-68px)]'>
-      Home
+    <div className='pt-24'>
+      <Banner />
     </div>
   </>;
 }
