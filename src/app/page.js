351f847109fdd8ce965d30620e0b0f25350a2864
iamtanim0195 <sahadatmd195@gmail.com>
2024-01-24 17:23:27 +0600

    solve marge Conflict tabproperty

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,10 @@
 import Banner from "@/components/Banner/Banner";
 import TabProperty from "@/components/TabPropety/TabProperty";
 export default function Home() {
-<<<<<<< HEAD
   return <>
     <div className='pt-24'>
       <Banner />
       <TabProperty />
     </div>
   </>;
-=======
-  return (
-    <>
-      <div>
-        <Banner />
-      </div>
-    </>
-  );
->>>>>>> 3a0ddf849c7a1619aa29c4597eff0a2bf7e7599a
 }
