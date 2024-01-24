e3810e45d4c0d3ff932f77e5708d342feac89197
adiyat-shihab <mohammadshihab104@gmail.com>
2024-01-25 00:36:04 +0600

    create tab system

--- src/components/TabPropety/TabProperty.js
+++ src/components/TabPropety/TabProperty.js
@@ -1,31 +1,44 @@
-import Link from 'next/link';
-import React from 'react';
-
+"use client";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import { useState } from "react";
 const TabProperty = () => {
-    const divisions = [
-        { "id": 1, "name": "Dhaka" },
-        { "id": 2, "name": "Chittagong" },
-        { "id": 3, "name": "Rajshahi" },
-        { "id": 4, "name": "Khulna" },
-        { "id": 5, "name": "Barisal" },
-        { "id": 6, "name": "Sylhet" },
-        { "id": 7, "name": "Rangpur" },
-        { "id": 8, "name": "Mymensingh" }
-    ];
+  const divisions = [
+    { id: 1, name: "Dhaka" },
+    { id: 2, name: "Chittagong" },
+    { id: 3, name: "Rajshahi" },
+    { id: 4, name: "Khulna" },
+    { id: 5, name: "Barisal" },
+    { id: 6, name: "Sylhet" },
+    { id: 7, name: "Rangpur" },
+    { id: 8, name: "Mymensingh" },
+  ];
 
-    return (
-        <div className="flex items-center justify-center py-5">
-            <ul className='flex gap-3'>
-                {divisions.map((division) => (
-                    <li key={division.id}>
-                        <button className="btn btn-secondary">
-                                {division.name}                          
-                        </button>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
-}
+  const [tabpanels, setTabpanels] = useState(undefined);
+
+  return (
+    <Tabs>
+      <TabList className="flex gap-3 items-center justify-center py-5">
+        {divisions.map((division) => (
+          <Tab key={division.id}>
+            <button
+              className="btn btn-secondary"
+              onClick={() => setTabpanels(division.name)}
+            >
+              {division.name}
+            </button>
+          </Tab>
+        ))}
+      </TabList>
+      <div>
+        <Tabpanelss data={tabpanels}></Tabpanelss>
+      </div>
+    </Tabs>
+  );
+};
 
 export default TabProperty;
+
+const Tabpanelss = ({ data }) => {
+  return <>{data}</>;
+};
