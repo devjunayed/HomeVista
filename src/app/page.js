827d46b70c32f787e91a02359edc5da1debaf906
adiyat-shihab <mohammadshihab104@gmail.com>
2024-01-26 17:07:19 +0600

    fix deploy

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,6 @@ import Banners from "@/components/Banner/Banners";
 import Brand from "@/components/Brand/Brand";
 import TabProperty from "@/components/TabPropety/TabProperty";
 export default async function Home() {
-  const data = await fetch("http://localhost:3000/api/get-property").then(
-    (res) => res.json(),
-  );
-  console.log(data);
-
   return (
     <>
       <div className="">
