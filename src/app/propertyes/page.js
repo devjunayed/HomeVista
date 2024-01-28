1ae717ddd90736e8a8fc4998f8b01d019454a652
adiyat-shihab <mohammadshihab104@gmail.com>
2024-01-28 20:54:42 +0600

    now adding the url for creatng the actual property

--- src/app/propertyes/page.js
+++ src/app/propertyes/page.js
@@ -2,40 +2,39 @@ import Link from "next/link";
 import React from "react";
 
 const page = async () => {
-  // const data = await fetch("https://home-vista.vercel.app/api/property", {
-  //   method: "GET",
-  // }).then((res) => res.json());
-  // console.log(data);
+  const data = await fetch("https://home-vista.vercel.app/api/property", {
+    method: "GET",
+  }).then((res) => res.json());
 
   return (
     <div>
       <ul className="flex gap-3 flex-wrap p-5">
-        {/*{data?.message?.map((item) => (*/}
-        {/*  <li key={item._id}>*/}
-        {/*    <div className="card w-96 bg-base-100 shadow-xl">*/}
-        {/*      <figure>*/}
-        {/*        <img src={item.photoUrl} alt="property" />*/}
-        {/*      </figure>*/}
-        {/*      <div className="card-body flex">*/}
-        {/*        <h2 className="card-title">*/}
-        {/*          {item.title}*/}
-        {/*          <div className="badge badge-secondary">NEW</div>*/}
-        {/*        </h2>*/}
-        {/*        <p>{item.description}</p>*/}
-        {/*        <p>{item.division}</p>*/}
-        {/*        <p>{item.district}</p>*/}
-        {/*        <div className="card-actions justify-end">*/}
-        {/*          <Link*/}
-        {/*            href={`property/${item._id}`}*/}
-        {/*            className="btn btn-primary"*/}
-        {/*          >*/}
-        {/*            Details*/}
-        {/*          </Link>*/}
-        {/*        </div>*/}
-        {/*      </div>*/}
-        {/*    </div>*/}
-        {/*  </li>*/}
-        {/*))}*/}
+        {data?.message?.map((item) => (
+          <li key={item._id}>
+            <div className="card w-96 bg-base-100 shadow-xl">
+              <figure>
+                <img src={item.photoUrl} alt="property" />
+              </figure>
+              <div className="card-body flex">
+                <h2 className="card-title">
+                  {item.title}
+                  <div className="badge badge-secondary">NEW</div>
+                </h2>
+                <p>{item.description}</p>
+                <p>{item.division}</p>
+                <p>{item.district}</p>
+                <div className="card-actions justify-end">
+                  <Link
+                    href={`property/${item._id}`}
+                    className="btn btn-primary"
+                  >
+                    Details
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </li>
+        ))}
       </ul>
     </div>
   );
