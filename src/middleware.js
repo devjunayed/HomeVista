9b04a0608d4e6c3aec2fa0a4f4e528841997bead
adiyat-shihab <mohammadshihab104@gmail.com>
2024-01-30 01:30:33 +0600

    fix middlewere

--- src/middleware.js
+++ src/middleware.js
@@ -1,18 +1,13 @@
 import { NextResponse } from "next/server";
 
 export async function middleware(request) {
-  let token = request.cookies.get("token");
+  const token = request.cookies.get("token");
   console.log(token);
   if (!token) {
     return NextResponse.redirect(new URL("/register", request.url));
-  } else if (
-    token &&
-    token.value &&
-    request.nextUrl.pathname === "/add-property"
-  ) {
-    return NextResponse.rewrite(new URL("/add-property", request.url));
+  } else {
+    return NextResponse.rewrite(new URL(request.url));
   }
-  return NextResponse.next();
 }
 
 export const config = {
