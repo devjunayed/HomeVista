3fea9f8801031bfdb0c1e92e362dbd8c49aab385
adiyat-shihab <mohammadshihab104@gmail.com>
2024-01-30 01:35:24 +0600

    change the rewrite to redirect

--- src/middleware.js
+++ src/middleware.js
@@ -2,11 +2,10 @@ import { NextResponse } from "next/server";
 
 export async function middleware(request) {
   const token = request.cookies.get("token");
-  console.log(token);
   if (!token) {
-    return NextResponse.rewrite(new URL("/register", request.url));
+    return NextResponse.redirect(new URL("/register", request.url));
   } else {
-    return NextResponse.rewrite(new URL(request.url));
+    return NextResponse.redirect(new URL(request.url));
   }
 }
 
