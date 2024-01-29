8e52e673ee838fe1d6d62bd28b980427ce37b360
adiyat-shihab <mohammadshihab104@gmail.com>
2024-01-29 20:29:10 +0600

    remove mobile nav signout in main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,12 @@
 import Banners from "@/components/Home/Banner/Banners";
 import Brand from "@/components/Home/Brand/Brand";
-
 import WhoWeAre from "@/components/Home/Who we are/WhoWeAre";
 import OurService from "@/components/Home/Our Service/OurService";
-import NavbarSignOut from "@/components/Nav/NavbarSignOut";
-import { onAuthStateChanged } from "firebase/auth";
-import auth from "../../firebase.config";
-import { NextResponse } from "next/server";
-
-function MobileBanner() {
-  return null;
-}
 
 export default async function Home() {
   return (
     <>
       <div className="">
-        <NavbarSignOut />
         <Banners />
         <Brand />
         <WhoWeAre />
