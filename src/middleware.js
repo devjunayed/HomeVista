3ffcfdffffe184bab7242849c36424afc59325b2
adiyat-shihab <mohammadshihab104@gmail.com>
2024-01-30 01:53:32 +0600

    middleware onek jala dise

--- src/middleware.js
+++ src/middleware.js
@@ -2,14 +2,13 @@ import { NextResponse } from "next/server";
 
 export async function middleware(request) {
   const token = request.cookies.get("token");
-  console.log(request);
-  if (!token && !token?.value) {
-    return NextResponse.redirect(new URL("/register", request.url));
-  } else {
+  if (token && token.value) {
     return NextResponse.next();
+  } else {
+    return NextResponse.redirect(new URL("/register", request.url));
   }
 }
 
 export const config = {
-  matcher: "/add-property",
+  matcher: ["/add-property", "/contact"],
 };
