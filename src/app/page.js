c288a1f0f547c4788fc720dea36aeae839cc283e
adiyat-shihab <mohammadshihab104@gmail.com>
2024-01-29 22:29:01 +0600

    change the database uri into env

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ export default async function Home() {
         <Banners />
         <Brand />
         <WhoWeAre />
-        {/*    property section*/}
+        {/*property section*/}
         <OurService />
       </div>
     </>
--- src/database/mongodbConnect.js
+++ src/database/mongodbConnect.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 const MongodbConnect = async () => {
   try {
-    await mongoose.connect(
-      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hzlter2.mongodb.net/homeVistaDB?retryWrites=true&w=majority`,
-    );
+    await mongoose.connect(process.env.DATABASE_URI);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log(error);
@@ -12,4 +10,3 @@ const MongodbConnect = async () => {
 };
 
 export default MongodbConnect;
-// mongodb+srv://<username>:<password>@cluster0.hzlter2.mongodb.net/?retryWrites=true&w=majority
