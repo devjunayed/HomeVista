273b76889d81fc057992a873ecfbec5b069f5e91
adiyat-shihab <mohammadshihab104@gmail.com>
2024-01-29 20:27:17 +0600

    adding middleware

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,20 @@ import Brand from "@/components/Home/Brand/Brand";
 
 import WhoWeAre from "@/components/Home/Who we are/WhoWeAre";
 import OurService from "@/components/Home/Our Service/OurService";
+import NavbarSignOut from "@/components/Nav/NavbarSignOut";
+import { onAuthStateChanged } from "firebase/auth";
+import auth from "../../firebase.config";
+import { NextResponse } from "next/server";
+
+function MobileBanner() {
+  return null;
+}
+
 export default async function Home() {
   return (
     <>
       <div className="">
+        <NavbarSignOut />
         <Banners />
         <Brand />
         <WhoWeAre />
