40e63fd0c517d0bb6f755b1bf21a9eab4c5fc800
adiyat-shihab <mohammadshihab104@gmail.com>
2024-01-30 15:32:01 +0600

    dynamic property data

--- src/app/add-property/page.js
+++ src/app/add-property/page.js
@@ -1,13 +1,18 @@
 "use client";
 import React, { useState } from "react";
-import { Button, Checkbox, ConfigProvider, message, Steps, theme } from "antd";
-import TextArea from "antd/es/input/TextArea";
+import { Button, Checkbox, message, Select, Steps } from "antd";
+import divisions from "@/lib/divisions";
+import districts from "@/lib/districts";
 
 const CreateProperty = () => {
   const [current, setCurrent] = useState(0);
   const [rentCheckbox, setrentCheckbox] = useState(false);
   const [saleCheckbox, setsaleCheckbox] = useState(false);
-  console.log(rentCheckbox);
+  const [division, setDivision] = useState("Dhaka");
+  const [district, setDistrict] = useState("");
+  const [place, setPlace] = useState([]);
+  const [selectedPlaces, setSelectedPlaces] = useState("");
+  console.log(place);
   const steps = [
     {
       title: "Property Title",
@@ -76,7 +81,140 @@ const CreateProperty = () => {
     },
     {
       title: "Property Address",
-      content: "Second-content",
+      content: (
+        <>
+          <div>
+            <div className="pb-2 pt-4">
+              <h4
+                className={
+                  "text-left text-[0.875rem] font-normal text-[#4F4F4F]"
+                }
+              >
+                Property Street
+              </h4>
+              <input
+                type="text"
+                name="property_street"
+                id="property_street"
+                className="block focus:shadow-md transition border border-[#CACACA]  h-[3rem] w-full outline-none p-4 text-lg rounded-[0.125rem]"
+                required
+              />
+            </div>
+            <div className="pb-2 pt-4 flex items-center justify-between">
+              <div>
+                <h4
+                  className={
+                    "text-left text-[0.875rem] font-normal text-[#4F4F4F]"
+                  }
+                >
+                  Division
+                </h4>
+
+                <Select
+                  defaultValue={division}
+                  style={{ width: 120 }}
+                  options={divisions}
+                  onChange={(value) => {
+                    setDistrict("");
+                    setSelectedPlaces("");
+                    setDivision(value);
+                  }}
+                />
+              </div>
+              <div>
+                <h4
+                  className={
+                    "text-left text-[0.875rem] font-normal text-[#4F4F4F]"
+                  }
+                >
+                  District
+                </h4>
+
+                <Select
+                  style={{ width: 120 }}
+                  options={districts[division]}
+                  value={district}
+                  onChange={async (value) => {
+                    setPlace([]);
+                    setDistrict(value);
+                    const where = encodeURIComponent(
+                      JSON.stringify({
+                        adminName2: value,
+                      }),
+                    );
+                    const data = await fetch(
+                      `https://parseapi.back4app.com/classes/BD?where=${where}`,
+                      {
+                        headers: {
+                          "X-Parse-Application-Id":
+                            "zS2XAEVEZAkD081UmEECFq22mAjIvX2IlTYaQfai", // This is the fake app's application id
+                          "X-Parse-Master-Key":
+                            "t6EjVCUOwutr1ruorlXNsH3Rz65g0jiVtbILtAYU", // This is the fake app's readonly master key
+                        },
+                      },
+                    );
+                    const res = await data.json();
+
+                    const updatedPlace = [];
+                    res.results.map((item) => {
+                      const exist = updatedPlace.find(
+                        (data) => data.value === item.adminName3,
+                      );
+                      if (!exist) {
+                        updatedPlace.push({
+                          value: item.adminName3,
+                          label: item.adminName3,
+                        });
+                      }
+                    });
+                    setPlace(updatedPlace);
+                  }}
+                />
+              </div>
+              <div>
+                <h4
+                  className={
+                    "text-left text-[0.875rem] font-normal text-[#4F4F4F]"
+                  }
+                >
+                  Place
+                </h4>
+
+                <Select
+                  style={{ width: 120 }}
+                  options={place}
+                  value={selectedPlaces}
+                  onChange={(value) => setSelectedPlaces(value)}
+                />
+              </div>
+            </div>
+            <div className="pb-2 pt-4 flex items-center justify-between">
+              <Checkbox
+                checked={rentCheckbox}
+                onChange={(e) => {
+                  setsaleCheckbox(false);
+                  setrentCheckbox(e.target.checked);
+                }}
+              >
+                <span className={" text-[0.875rem] font-normal text-[#4F4F4F]"}>
+                  Rent
+                </span>
+              </Checkbox>
+              <Checkbox
+                checked={saleCheckbox}
+                onChange={(e) => {
+                  setrentCheckbox(false);
+                  setsaleCheckbox(e.target.checked);
+                }}
+              >
+                <p className={" text-[0.875rem] font-normal text-[#4F4F4F]"}>
+                  Sell
+                </p>
+              </Checkbox>
+            </div>
+          </div>
+        </>
+      ),
     },
     {
       title: "Publish",
@@ -93,6 +231,7 @@ const CreateProperty = () => {
     key: item.title,
     title: item.title,
   }));
+
   return (
     <>
       <Steps
@@ -110,7 +249,9 @@ const CreateProperty = () => {
               className={
                 "px-6 py-2 rounded mt-4  bg-[#3A0CA3] text-white text-[0.875rem] font-normal  "
               }
-              onClick={() => next()}
+              onClick={() => {
+                next();
+              }}
             >
               Next
             </button>
--- src/lib/divisions.js
+++ src/lib/divisions.js
@@ -1,12 +1,11 @@
 const divisions = [
-  "Barisal",
-  "Chittagong",
-  "Dhaka",
-  "Khulna",
-  "Mymensingh",
-  "Rajshahi",
-  "Rangpur",
-  "Sylhet",
+  { value: "Barisal", label: "Barisal" },
+  { value: "Chittagong", label: "Chittagong" },
+  { value: "Dhaka", label: "Dhaka" },
+  { value: "Khulna", label: "Khulna" },
+  { value: "Mymensingh", label: "Mymensingh" },
+  { value: "Rajshahi", label: "Rajshahi" },
+  { value: "Rangpur", label: "Rangpur" },
+  { value: "Sylhet", label: "Sylhet" },
 ];
-
 export default divisions;
