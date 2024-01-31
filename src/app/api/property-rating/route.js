ede018033eb9cb89a575fe12e64943e296c5f1da
adiyat-shihab <mohammadshihab104@gmail.com>
2024-02-01 02:52:01 +0600

    two times get method

--- src/app/api/property-rating/route.js
+++ src/app/api/property-rating/route.js
@@ -22,13 +22,11 @@ export async function PUT(req, res) {
     } else {
       await ratingModel.create(data);
       return NextResponse.json({ message: "Rating created" });
-
     }
   } catch (err) {
     console.log(err);
   }
 }
-
 
 export async function GET(req, res) {
   try {
@@ -40,40 +38,13 @@ export async function GET(req, res) {
 
     await MongodbConnect();
 
-    const query = {propertyId, userId};
+    const query = { propertyId, userId };
 
     const isRated = await ratingModel.findOne(query);
 
     if (isRated) {
       console.log(isRated.rating, "this is rating");
-      return NextResponse.json({data: isRated.rating});
-    }
-    return NextResponse.json({ data: 5 });
-  } catch (err) {
-    console.log(err);
-    return NextResponse.json({ message: "something went wrong" });
-
-  }
-}
-
-
-export async function GET(req, res) {
-  try {
-    const propertyId = req.nextUrl.searchParams.get("propertyId");
-    const userId = req.nextUrl.searchParams.get("userId");
-
-    console.log(propertyId);
-    console.log(userId);
-
-    await MongodbConnect();
-
-    const query = {propertyId, userId};
-
-    const isRated = await ratingModel.findOne(query);
-
-    if (isRated) {
-      console.log(isRated.rating, "this is rating");
-      return NextResponse.json({data: isRated.rating});
+      return NextResponse.json({ data: isRated.rating });
     }
     return NextResponse.json({ data: 5 });
   } catch (err) {
