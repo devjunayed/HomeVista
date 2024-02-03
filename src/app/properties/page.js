e898ca8bc756e5133b776d6fa9e786f501fca6ef
iamtanim0195 <sahadatmd195@gmail.com>
2024-02-03 21:21:38 +0600

    propertyesAPI post/get/delete/put/dynamic route created

--- src/app/properties/page.js
+++ src/app/properties/page.js
@@ -7,3 +7,10 @@ const page = () => {
 }
 
 export default page
+
+const property = async () => {
+  const data = await fetch(`http://localhost:3000/api/property`, {
+    next: { tags: ["property"], revalidate: 1 },
+  });
+  return data.json();
+};
