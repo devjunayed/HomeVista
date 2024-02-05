19a399c706e27d906cbbaef31ab0a22db25fad19
Md Junayed <devjunayed@gmail.com>
2024-02-05 14:06:25 +0600

    some changes

--- src/app/properties/page.js
+++ src/app/properties/page.js
@@ -1,23 +1,37 @@
-import ListProperty from "@/components/ListProperty/ListProperty";
-import doFetch from "@/lib/doFetch";
-import React from "react";
+import React from 'react'
 
-const Page = async () => {
-  const propertyData = await GetProperty();
-  return <ListProperty data={propertyData} />;
-};
+const Page = () => {
+  return (
+    <div>
+      page
+    </div>
+  )
+}
 
-export default Page;
+export default Page
 
-const GetProperty = async () => {
-  try {
-    const res = await doFetch("/properties");
-    const data = await res.json();
-    console.log('null from where');
-    console.log(data);
-    return data;
-  } catch (error) {
-    console.error("Error fetching properties", error);
-    throw error; // Propagate the error
-  }
-};
+
+
+// import ListProperty from "@/components/ListProperty/ListProperty";
+// import doFetch from "@/lib/doFetch";
+// import React from "react";
+
+// const Page = async () => {
+//   const propertyData = await GetProperty();
+//   return <ListProperty data={propertyData} />;
+// };
+
+// export default Page;
+
+// const GetProperty = async () => {
+//   try {
+//     const res = await doFetch("/properties");
+//     const data = await res.json();
+//     console.log('null from where');
+//     console.log(data);
+//     return data;
+//   } catch (error) {
+//     console.error("Error fetching properties", error);
+//     throw error; // Propagate the error
+//   }
+// };
