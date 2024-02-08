f2195c4cc5a0fa9efe31b734196adc2446b3ae4e
Md Junayed <devjunayed@gmail.com>
2024-02-08 17:53:50 +0600

    connected api to the frontend for add to favourite features

--- src/app/api/add-to-favourite/route.js
+++ src/app/api/add-to-favourite/route.js
@@ -2,20 +2,77 @@ import MongodbConnect from "@/database/mongodbConnect";
 import favModel from "@/model/favouriteProperty";
 import { NextResponse } from "next/server";
 
-export async function PUT(req) {
+// Get API to check if property is already favourite
+export async function GET(req) {
   try {
+    // getting ids from search params
     const propertyId = req.nextUrl.searchParams.get("propertyId");
     const userId = req.nextUrl.searchParams.get("userId");
+
+    // connecting to the database
     MongodbConnect();
+
+    // query for checking if id is in the database
     const query = { propertyId, userId };
+
+    // finding user favourite for this property
+    const isExist = await favModel.findOne(query);
+    const allFav = await favModel.find({ propertyId });
+    const allFavLength = allFav.length;
+
+    // sending response for result
+    if (isExist) {
+      return NextResponse.json({
+        favCount: allFavLength,
+        isFound: true,
+        status: "ok",
+      });
+    }
+    return NextResponse.json({
+      favCount: allFavLength,
+      isFound: false,
+      status: "!ok",
+    });
+  } catch (err) {
+    // catching err if one
+    console.log(err);
+    return NextResponse.json({ message: "something wrong" });
+  }
+}
+
+// PUT API for adding property to favourite
+export async function PUT(req) {
+  try {
+    // getting search params from url
+    const propertyId = req.nextUrl.searchParams.get("propertyId");
+    const userId = req.nextUrl.searchParams.get("userId");
+
+    // connecting to the database
+    MongodbConnect();
+
+    // query for checking if the data is already inserted
+    const query = { propertyId, userId };
+
+    // searching for data in database
     const isFavExist = await favModel.findOne(query);
 
+    // if data is not available then create one else send message
     if (!isFavExist) {
-      await favModel.create({ propertyId, userId });
-      return NextResponse.json({ status: "ok", message: "Added to favourite" });
+      const res = await favModel.create({ propertyId, userId });
+      if (res) {
+        return NextResponse.json({
+          status: "ok",
+          message: "Added to favourite",
+        });
+      }
     } else {
-        console.log('yo');
-      return NextResponse.json({ message: "Already added to favourite" });
+      const res = await favModel.findOneAndDelete(query);
+      if (res) {
+        return NextResponse.json({
+          status: "ok",
+          message: "Removed from favourite",
+        });
+      }
     }
   } catch (err) {
     console.log(err);
@@ -23,17 +80,28 @@ export async function PUT(req) {
   }
 }
 
-export async function DELETE(req) {
-  const propertyId = req.nextUrl.searchParams.get("propertyId");
-  const userId = req.nextUrl.searchParams.get("userId");
-  MongodbConnect();
-  const res = await favModel.findOneAndDelete({ propertyId, userId });
+// // DELETE API for un favourite a property
+// export async function DELETE(req) {
 
-  if (res) {
-    return NextResponse.json({
-      deleted: true,
-      message: "Removed form favourite",
-    });
-  }
-  return NextResponse.json({ message: "property not found" });
-}
+//   // getting ID's from search params in url
+//   const propertyId = req.nextUrl.searchParams.get("propertyId");
+//   const userId = req.nextUrl.searchParams.get("userId");
+
+//   // Connecting to the database
+//   MongodbConnect();
+
+//   // deleting data from database
+//   const res = await favModel.findOneAndDelete({ propertyId, userId });
+
+//   // if delted than send response
+//   if (res) {
+//     return NextResponse.json({
+//       status: "ok",
+//       deleted: true,
+//       message: "Removed form favourite",
+//     });
+//   }
+
+//   // if not data deleted than send response
+//   return NextResponse.json({ message: "property not found" });
+// }
--- src/components/AddToFav/AddToFav.jsx
+++ src/components/AddToFav/AddToFav.jsx
@@ -1,17 +1,67 @@
-import React from 'react'
-import { FaRegHeart } from 'react-icons/fa'
+"use client";
 
-const AddToFav = ({userId, propertyId}) => {
+import { message } from 'antd';
+import React, { useEffect } from 'react'
+import { LuHeart, LuHeartOff } from "react-icons/lu";
+import useSWR, { mutate } from 'swr';
 
-    const handleAddToFav = () => {
-        
+const AddToFav = ({ userId, propertyId, setFavourite }) => {
+    const [messageApi, contextHolder] = message.useMessage();
+
+    const url = `/api/add-to-favourite?userId=${userId}&propertyId=${propertyId}`;
+
+    const { data: favData, error } = useSWR(url, getFav);
+
+    useEffect(() => {
+        if (favData) {
+            setFavourite(() => favData?.favCount);
+        }
+    }, [favData]);
+
+    const handleFav = () => {
+        if (favData && favData.isFound !== undefined) {
+            fetch(url, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data?.status === "ok") {
+                        messageApi.open({
+                            type: "success",
+                            content: data.message,
+                        });
+                        mutate(url);
+                    }
+
+                });
+        }
+
     }
-
     return (
-        <button onClick={handleAddToFav} className=" btn bg-secondary hover:bg-blue-800 text-white text-xl flex items-center justify-center gap-2 py-2">
-            <FaRegHeart /> Favourite
-        </button>
+        <>
+            <button onClick={handleFav} className=" btn bg-secondary hover:bg-blue-800 text-white text-xl flex items-center justify-center gap-2 py-2">
+                {contextHolder}
+                {
+                    favData?.isFound ? <><LuHeartOff /> Unfavourite </> :
+                        <><LuHeart /> Favourite</>
+                }
+            </button>
+        </>
     )
 }
 
-export default AddToFav
+
+const getFav = async (url) => {
+    const res = await fetch(url, {
+        method: "GET"
+    });
+
+    const result = await res.json();
+
+    return result;
+}
+
+export default AddToFav;
