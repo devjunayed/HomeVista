d86803260acd5c01d6dc7094dc6b18609abd5fe5
Md Junayed <devjunayed@gmail.com>
2024-02-09 23:32:29 +0600

    some fix

--- src/app/api/add-to-favourite/route.js
+++ src/app/api/add-to-favourite/route.js
@@ -2,80 +2,85 @@ import MongodbConnect from "@/database/mongodbConnect";
 import favModel from "@/model/favouriteProperty";
 import { NextResponse } from "next/server";
 
-// Get API to check if property is already favourite
+// Get API to check if property is already favorite
 export async function GET(req) {
   try {
-    // getting ids from search params
+    // Getting ids from search params
     const propertyId = req.nextUrl.searchParams.get("propertyId");
     const userId = req.nextUrl.searchParams.get("userId");
 
-    // connecting to the database
+    // Connecting to the database
     MongodbConnect();
 
-    // query for checking if id is in the database
+    // Query for checking if id is in the database
     const query = { propertyId, userId };
 
-    // finding user favourite for this property
+    // Finding user favorite for this property
     const isExist = await favModel.findOne(query);
     const allFav = await favModel.find({ propertyId });
     const allFavLength = allFav.length;
 
-    // sending response for result
+    // Sending response for result
     if (isExist) {
       return NextResponse.json({
         favCount: allFavLength,
         isFound: true,
         status: "ok",
       });
+    } else {
+      return NextResponse.json({
+        favCount: allFavLength,
+        isFound: false,
+        status: "ok",
+      });
     }
-    return NextResponse.json({
-      favCount: allFavLength,
-      isFound: false,
-      status: "!ok",
-    });
   } catch (err) {
-    // catching err if one
-    console.log(err);
-    return NextResponse.json({ message: "something wrong" });
+    // Catching err if one
+    console.error("GET request error:", err);
+    return NextResponse.error(500, "Internal Server Error");
   }
 }
 
-// PUT API for adding property to favourite
+// PUT API for adding property to favorite
 export async function PUT(req) {
   try {
-    // getting search params from url
+    // Getting search params from url
     const propertyId = req.nextUrl.searchParams.get("propertyId");
     const userId = req.nextUrl.searchParams.get("userId");
 
-    // connecting to the database
+    // Connecting to the database
     MongodbConnect();
 
-    // query for checking if the data is already inserted
+    // Query for checking if the data is already inserted
     const query = { propertyId, userId };
 
-    // searching for data in database
+    // Searching for data in database
     const isFavExist = await favModel.findOne(query);
 
-    // if data is not available then create one else send message
+    // If data is not available then create one else send message
     if (!isFavExist) {
       const res = await favModel.create({ propertyId, userId });
       if (res) {
         return NextResponse.json({
           status: "ok",
-          message: "Added to favourite",
+          message: "Added to favorite",
         });
+      } else {
+        return NextResponse.error(500, "Failed to add to favorite");
       }
     } else {
       const res = await favModel.findOneAndDelete(query);
       if (res) {
         return NextResponse.json({
           status: "ok",
-          message: "Removed from favourite",
+          message: "Removed from favorite",
         });
+      } else {
+        return NextResponse.error(500, "Failed to remove from favorite");
       }
     }
   } catch (err) {
-    console.log(err);
-    return NextResponse.json({ message: err });
+    console.error("PUT request error:", err);
+    return NextResponse.error(500, "Internal Server Error");
   }
 }
