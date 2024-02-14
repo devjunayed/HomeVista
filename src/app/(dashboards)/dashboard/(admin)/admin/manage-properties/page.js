4863dd582af526ac3a15be5437471c0d99a6f3d3
Md Junayed <devjunayed@gmail.com>
2024-02-15 00:44:23 +0600

    protecting admin

--- src/app/(dashboards)/dashboard/(admin)/admin/manage-properties/page.js
+++ src/app/(dashboards)/dashboard/(admin)/admin/manage-properties/page.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import ProtectAdmin from "@/components/admin-components/ProtectAdmin/ProtectAdmin";
+import React from "react";
 
 const page = () => {
   return (
-    <div>
-      Manage Properties
-    </div>
-  )
-}
+    <ProtectAdmin>
+      <div>Manage Properties</div>
+    </ProtectAdmin>
+  );
+};
 
-export default page
+export default page;
