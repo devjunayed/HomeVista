321e0269b10818611539d8036691308391bc9a4d
Md Junayed <devjunayed@gmail.com>
2024-02-18 22:05:41 +0600

    manage property data is shown

--- src/app/(dashboards)/dashboard/(admin)/admin/manage-properties/page.js
+++ src/app/(dashboards)/dashboard/(admin)/admin/manage-properties/page.js
@@ -1,12 +1,40 @@
-import ProtectAdmin from "@/components/admin-components/ProtectAdmin/ProtectAdmin";
+"use client";
+import ListProperty from "@/components/admin-components/ListPropertyAdmin/ListPropertyAdmin";
+import SearchForm from "@/components/ui-components/SearchForm/SearchForm";
 import React from "react";
+import useSWR from "swr";
 
-const Page = () => {
+const Page = ({ searchParams }) => {
+  const url = `/api/properties?title=${searchParams.title}`;
+  const { data, error, mutate } = useSWR(url, GetProperty);
+  if (data) {
+    return (
+      <>
+        <SearchForm />
+        <div className="flex">
+          <div className="w-full h-[76vh] overflow-scroll">
+            <ListProperty data={data} />;
+          </div>
+        </div>
+      </>
+    );
+  }
   return (
-    <ProtectAdmin>
-      <div>Manage Properties</div>
-    </ProtectAdmin>
+    <div className="min-h-[90vh] flex items-center justify-center">
+      <span className="loading loading-bars loading-lg"></span>
+    </div>
   );
 };
 
 export default Page;
+
+const GetProperty = async (url) => {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data.data;
+  } catch (error) {
+    console.error("Error fetching properties", error);
+    throw error; // Propagate the error
+  }
+};
